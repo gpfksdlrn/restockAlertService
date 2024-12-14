be084c94cacbed052ce5fc5cc7c9d2ca596d133f
hizdev <gpfksdlrn@naver.com>
2024-12-14 09:59:20 +0900

    init

--- src/main/java/com/restock/RestockApplication.java
+++ src/main/java/com/restock/RestockApplication.java
@@ -1,4 +1,4 @@
-package java.com.restock;
+package com.restock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/restock/RestockApplicationTests.java
+++ src/test/java/com/restock/RestockApplicationTests.java
@@ -1,4 +1,4 @@
-package java.com.restock;
+package com.restock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
